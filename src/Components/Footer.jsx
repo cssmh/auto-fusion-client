932a9d58a2c225d2ba6d5e2e97235b8604d5fa99
Momen <touristmomen@gmail.com>
2025-03-30 05:09:05 +0600

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,18 +6,13 @@ const Footer = () => {
   return (
     <div className="bg-main py-10 text-center text-white">
       <div className="container mx-auto">
-        {/* Logo and Title */}
         <div className="flex justify-center items-center gap-3">
           <FaCar className="text-4xl" />
           <p className="text-2xl font-bold">Auto Fusion</p>
         </div>
-
-        {/* Copyright Text */}
         <p className="mt-3 text-sm">
           &copy; {currentYear} Auto Fusion. All rights reserved.
         </p>
-
-        {/* Social Icons */}
         <div className="flex justify-center gap-5 mt-4">
           <a
             href="https://facebook.com"
